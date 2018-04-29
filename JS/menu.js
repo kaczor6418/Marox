43827c5867198bfa9cb9d5b3cfb9a420aea899d8
kaczor6418 <andrzej6418@gmail.com>
2018-04-29 12:04:22 +0200

    add scroll top button

--- JS/menu.js
+++ JS/menu.js
@@ -1,4 +1,8 @@
+(function() {
+
 var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function() {
 	document.querySelector(".page-header").classList.toggle("navi-opened");
 }, false);
+
+})();
