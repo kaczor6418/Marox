482da9238ad52cba1e3a367ed968f4a3c20ac6f7
krzysztof.kaczynski <andrzej6418@gmail.com>
2018-08-02 15:45:48 +0200

    change names of files

--- JS/validation.js
+++ JS/validation.js
@@ -12,7 +12,8 @@
     }
     
     function isEmail(field) {
-      return field.value.indexOf("@") !== -1;
+      let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(field).toLowerCase());
     }
     
     function displayErrors(errors) {
